923c3ec6a600170f3d57d0a744ea54669c3f261b
manoj <manojkolagana25@gmail.com>
2026-03-11 13:03:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GitHub Reusable Workflow Demo!"
+    return "Hello from GitHub DevOps Pipeline"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
